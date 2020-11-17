6787d14efbe3c30e566bf903f34de44cabe16c88
Athaariq Ardhiansyah <athaariqa@gmail.com>
2020-11-17 10:27:53 +0700

    test: Add codecov.io integration

--- test_all.sh
+++ test_all.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -e
 
+rm -f coverage.txt
+
 dialects=("postgres" "postgres_simple" "mysql" "mysql-legacy" "mariadb" "sqlite") # "mssql")
 
 for dialect in "${dialects[@]}" ; do
   if [ "$GORM_DIALECT" = "" ] || [ "$GORM_DIALECT" = "${dialect}" ]
   then
-    GORM_DIALECT=${dialect} go test  --tags "json1"
+    GORM_DIALECT=${dialect} go test -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
   fi
 done
